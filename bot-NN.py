1b4916a7c4c216157e720315d91ffe983faa62c9
tagirsabirov <tagirsabirov1@gmail.com>
2022-04-05 22:24:38 -0700

    Change format

--- bot-NN.py
+++ bot-NN.py
@@ -1,6 +1,62 @@
 import chess
 from evaluation import Evaluate
+from tensorflow import keras
+import numpy as np
+model = keras.models.load_model('NN/NN-model.h5')
 
+squares_index = {
+  'a': 0,
+  'b': 1,
+  'c': 2,
+  'd': 3,
+  'e': 4,
+  'f': 5,
+  'g': 6,
+  'h': 7
+}
+
+def square_to_index(square):
+  letter = chess.square_name(square)
+  return 8 - int(letter[1]), squares_index[letter[0]]
+
+def split_dims(board):
+    # this is the 3d matrix
+    board3d = np.zeros((14, 8, 8), dtype=np.int8)
+
+    # here we add the pieces's view on the matrix
+    for piece in chess.PIECE_TYPES:
+      for square in board.pieces(piece, chess.WHITE):
+        idx = np.unravel_index(square, (8, 8))
+        board3d[piece - 1][7 - idx[0]][idx[1]] = 1
+      for square in board.pieces(piece, chess.BLACK):
+        idx = np.unravel_index(square, (8, 8))
+        board3d[piece + 5][7 - idx[0]][idx[1]] = 1
+
+    # add attacks and valid moves too
+    # so the network knows what is being attacked
+    aux = board.turn
+    board.turn = chess.WHITE
+    for move in board.legal_moves:
+        i, j = square_to_index(move.to_square)
+        board3d[12][i][j] = 1
+    board.turn = chess.BLACK
+    for move in board.legal_moves:
+        i, j = square_to_index(move.to_square)
+        board3d[13][i][j] = 1
+    board.turn = aux
+
+    return board3d
+
+def neural_eval(board):
+    board3d = split_dims(board)
+    board3d = np.expand_dims(board3d, 0)
+    board3d = np.asarray(board3d).astype(np.float32)
+    return model.predict(board3d)[0][0]
+
+
+# create board object
+board = chess.Board()
+legal_moves = list(board.legal_moves)
 
 class chessbot:
     def minimax(board, depth, maximizing_player, alpha, beta):
@@ -10,7 +66,7 @@ class chessbot:
         if depth == 0 or board.is_game_over():
             #Only calculate the evaluation once per node, at the lowest depth of the tree
             # return Evaluate.evaluate_board(board), None
-            return Evaluate.evaluate_board(board);
+            return neural_eval(board)
             
         if maximizing_player:
             #value is the board value
--- steps.md
+++
@@ -1,5 +0,0 @@
-# Todo:
-
-1. Make eval function better
-2. Add book moves
-3. (optional): Add Neural Network evaluation
